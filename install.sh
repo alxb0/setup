2776e07784b41d4d6480c439ec21932bfec586e5
Alexandre Boucher <alexandre.boucher1@gmail.com>
2017-06-18 18:23:34 +0200

    Add repository of google-chrome-stable to install it

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 CURDIR=$(pwd)
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 
 sudo apt-get update
 sudo apt-get dist-upgrade -y
